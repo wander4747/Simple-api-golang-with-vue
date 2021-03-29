cca0b0a03beab545a2fc4164b007035c28e06534
Wander <wander@bigbangcomunicacao.com.br>
2021-03-29 20:23:32 -0300

    feat: config gorilla mux and server

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
 
+	router := mux.NewRouter()
+
+	fmt.Println("Run port 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
