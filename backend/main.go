3176b677171bddf828554e4d30b75ce0a6e1eaad
Wander <wander@bigbangcomunicacao.com.br>
2021-03-30 17:12:26 -0300

    chore(main): added route update user

--- backend/main.go
+++ backend/main.go
@@ -16,6 +16,7 @@ func main() {
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
 	router.HandleFunc("/users", server.GetUsers).Methods(http.MethodGet)
 	router.HandleFunc("/users/{id}", server.GetUser).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.UpdateUser).Methods(http.MethodPut)
 
 	fmt.Println("Run port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
