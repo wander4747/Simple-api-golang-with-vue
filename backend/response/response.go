ddb8968be03cec82a848b64597709fce158044c6
Wander <wander@bigbangcomunicacao.com.br>
2021-03-30 12:41:47 -0300

    feat(response): create return generic json

---
+++ backend/response/response.go
@@ -0,0 +1,29 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON returns a JSON response for the request
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if data != nil {
+		if erro := json.NewEncoder(w).Encode(data); erro != nil {
+			log.Fatal(erro)
+		}
+	}
+
+}
+
+// Erro Error returns an error in JSON format
+func Erro(w http.ResponseWriter, statusCode int, erro error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: erro.Error(),
+	})
+}
