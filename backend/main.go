3dd2a482e87c27ff0f48ee657382fb3ba1f671be
Wander <wander@bigbangcomunicacao.com.br>
2021-03-30 12:42:41 -0300

    chore(main): added route post user

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/server"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +12,8 @@ import (
 func main() {
 
 	router := mux.NewRouter()
+
+	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
 
 	fmt.Println("Run port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
