56d8f1b6d6ab411add88f896d6aa3bc5899947b7
Wander <wander@bigbangcomunicacao.com.br>
2021-03-30 12:58:36 -0300

    chore(main): added route get user

--- backend/main.go
+++ backend/main.go
@@ -14,6 +14,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", server.GetUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.GetUser).Methods(http.MethodGet)
 
 	fmt.Println("Run port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
